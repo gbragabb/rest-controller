cc118417037fedd44364e77dbe62bbe5b22e17fb
Gabriel Braga Zamproni Lima <g.braga@bb.com.br>
2022-09-06 13:57:34 -0300

    remoção dos arquivos

--- proj-pedido-compras-quarkus/src/test/java/br/com/bb/compra/ClienteResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.bb.compra;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ClienteResourceIT extends ClienteResourceTest {
-
-    // Execute the same tests but in native mode.
-}
